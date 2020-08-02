cc12606eb75a037a9793068111be44974f3525f4
爱呦魏 <1976919313@qq.com>
2020-08-02 17:44:43 +0800

    退出登录问题修复

--- front/drawadmin/src/components/Login.vue
+++ front/drawadmin/src/components/Login.vue
@@ -53,10 +53,9 @@ export default {
                 const {data: res} = await this.$http.post("systemUser/login", this.loginForm);
                 if(res.code !== 0) return this.$message.error('登录失败');
                 this.$message.success('登录成功');
-                
-                this.$router.push('/home');
-                
+
                 window.sessionStorage.setItem('token', res.data.userName);
+                this.$router.push('/home');
             })
         }
     }
